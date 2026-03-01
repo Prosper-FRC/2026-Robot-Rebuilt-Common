796b9fad89e54517bb50156f4a570bd879366569
A_Dev484 <arnav.s.sutar@gmail.com>
2026-03-01 09:04:21 -0600

    AS - Auton commands changes
    no where near done lord help me

--- src/main/java/frc/robot/AutonCommands.java
+++ src/main/java/frc/robot/AutonCommands.java
@@ -4,19 +4,50 @@ package frc.robot;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 // imports
 
 public class AutonCommands {
+
     private final HashMap<String, Command> kCommandHashMap;
     private final ArrayList<Command> kQueuedCommands;
+
+    // We might need these?
+    private SendableChooser<Command> autoChooser;
+
+    /* someone please explain why we put m
+     * private Drive robotdrive;
+     * private Shooter mShooter;
+     * private Intake mIntake;
+     * private Indexer mIndexer;
+     * private Superstructure robotSuperstructure;
+     * 
+     */
+
 
     public AutonCommands() 
     {
         kCommandHashMap = new HashMap<String, Command>();
         kQueuedCommands = new ArrayList<Command>();
+
+        autoChooser = new SendableChooser<>();
     }
+
+    public void addCommandToQueue(String name) 
+    {
+        kQueuedCommands.add(kCommandHashMap.get(name));
+    }
+
+    public void runQueue()
+    {
+        
+    }
+
+    // Implement later
+    private Command getQueueAsSequentialCommand(ArrayList<Command> queue) { return new SequentialCommandGroup(null); }
 
 }
 
