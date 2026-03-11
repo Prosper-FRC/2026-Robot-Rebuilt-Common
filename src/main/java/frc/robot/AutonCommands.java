e9ffa6a6f306da9b1782fe7069f0b87b72803a1b
A_Dev484 <arnav.s.sutar@gmail.com>
2026-03-10 19:20:11 -0500

    AS - New paths with 2 "out of the way" options
    you can check documentiation for paths in naming_Choreo_Paths.txt
    
    ragave said that he pushed his auton chooser code but theres nothing to pull and there are no recent commits...
    
    did he just commit and not push?
    
    also i cant even merge drive and auton cuz drive isnt going to get pulled into production for ages prob
    
    the merge button is so tempting...
    we must stay strong brothers
    
    holy yap lil bro

--- src/main/java/frc/robot/AutonCommands.java
+++ src/main/java/frc/robot/AutonCommands.java
@@ -4,7 +4,6 @@ package frc.robot;
 import java.util.ArrayList;
 
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Subsystems.Drive.Drive;
