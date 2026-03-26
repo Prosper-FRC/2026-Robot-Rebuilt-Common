97994a61a9dcd86c5da38f1e5d5cf2b33d819c47
matthew-92 <dominmat002@gmail.com>
2026-03-25 19:34:25 -0500

    [MD] Strengthened ambiguity threshold

--- src/main/java/frc/robot/Subsystems/vision/visionConstants/VisionConstants5411.java
+++ src/main/java/frc/robot/Subsystems/vision/visionConstants/VisionConstants5411.java
@@ -4,7 +4,7 @@ import edu.wpi.first.wpilibj.RobotBase;
 public class VisionConstants5411 extends VisionConstants{
     public final String kCamName = "limelight-robo";
 
-    public final double kAmbiguityThreshold = (RobotBase.isReal()) ? 0.5 : 1.0;
+    public final double kAmbiguityThreshold = (RobotBase.isReal()) ? 0.2 : 1.0;
     public VisionConstants5411() {
 
     }
