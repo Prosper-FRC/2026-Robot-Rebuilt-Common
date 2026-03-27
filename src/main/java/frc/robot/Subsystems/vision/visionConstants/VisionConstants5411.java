3f104fcee735dab4ac5532c01193b1be6ddb8769
Matthew D <dominmat002@gmail.com>
2026-03-26 20:56:21 -0500

    [MD] FINISHED; vision constant logic error fixed

--- src/main/java/frc/robot/Subsystems/vision/visionConstants/VisionConstants5411.java
+++ src/main/java/frc/robot/Subsystems/vision/visionConstants/VisionConstants5411.java
@@ -2,10 +2,10 @@ package frc.robot.Subsystems.Vision.visionConstants;
 import edu.wpi.first.wpilibj.RobotBase;
 
 public class VisionConstants5411 extends VisionConstants{
-    public final String kCamName = "limelight-robo";
-
-    public final double kAmbiguityThreshold = (RobotBase.isReal()) ? 0.2 : 1.0;
+    
     public VisionConstants5411() {
+        kCamName = "limelight-robo";
 
+        kAmbiguityThreshold = (RobotBase.isReal()) ? 0.2 : 1.0;
     }
 }
